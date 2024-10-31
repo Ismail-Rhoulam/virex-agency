dc2b3ac7ecb9f60a70efbdb48a358625e2b700dd
Ismail RHOULAM <150340166+Ismail-Rhoulam@users.noreply.github.com>
2024-10-31 21:29:54 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -120,7 +120,7 @@ export default function Home() {
 				justifyContent="space-between">
 				<Text
 					variant="body-default-s" onBackground="neutral-weak">
-					© 2024 Once UI, <Link href="https://github.com/once-ui-system/nextjs-starter?tab=MIT-1-ov-file">MIT License</Link>
+					© 2024 VireX agency, All Rights Reserved
 				</Text>
 				<Flex
 					gap="12">
