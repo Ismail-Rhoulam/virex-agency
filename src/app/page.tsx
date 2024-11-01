fab03f72e77b74e6f7e8b8e4b1b664d904fc41e6
Ismail RHOULAM <150340166+Ismail-Rhoulam@users.noreply.github.com>
2024-11-01 02:38:52 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -119,7 +119,7 @@ export default function Home() {
 			<Flex
 				as="footer"
 				position="relative"
-				fillWidth paddingX="m" paddingY="m"
+				fillWidth paddingX="s" paddingY="m"
 				justifyContent="space-between">
 				<Text
 					variant="body-default-s" onBackground="neutral-weak">
