a38dc3abdce4131424530c7e520c80934cc2939b
Ismail RHOULAM <150340166+Ismail-Rhoulam@users.noreply.github.com>
2024-11-06 19:40:10 +0000

    Adding content to About/page.tsx and Work/page.tsx

---
+++ src/app/About/page.tsx
@@ -0,0 +1,16 @@
+import { Flex, Text } from "@/once-ui/components";
+
+
+export default function About() {
+    
+    const teext = "HAHAHA"
+
+    return (
+        <Flex>
+            <Text>
+                {teext}
+            </Text>
+
+        </Flex>
+    )
+}
---
+++ src/app/Work/page.tsx
@@ -0,0 +1,16 @@
+import { Flex, Text } from "@/once-ui/components";
+
+
+export default function Work() {
+
+    const teext = "HAHAHA"
+
+    return (
+        <Flex>
+            <Text>
+      å√          {teext}
+            </Text>
+
+        </Flex>
+    )
+}
