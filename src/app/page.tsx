39ffaf9ad201f42a6249575a1dec4f7c6315bb16
Ismail RHOULAM <150340166+Ismail-Rhoulam@users.noreply.github.com>
2024-11-07 10:02:08 +0000

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,27 +12,34 @@ export default function Home() {
     <Flex
       fillWidth paddingTop="l" paddingX="l"
       direction="column" alignItems="center" flex={1}>
+
       <Flex
         position="relative"
         as="section" overflow="hidden"
         fillWidth minHeight="0" maxWidth={68}
         direction="column" alignItems="center" flex={1}>
+
         <Flex
           as="main"
           direction="column" justifyContent="center"
           fillWidth fillHeight padding="l" gap="l">
+
           <Flex
             mobileDirection="column"
             fillWidth gap="24">
             <Flex
               position="relative"
               flex={2} paddingTop="56" paddingX="xl">
+
               <Logo size="l" icon={false} style={{zIndex: '1'}}/>
+
             </Flex>
+
             <Flex
               position="relative"
               flex={4} gap="24" marginBottom="104"
               direction="column">
+              
               <InlineCode
                 className="shadow-m"
                 style={{
@@ -41,26 +48,35 @@ export default function Home() {
                   backdropFilter: 'blur(var(--static-space-1))'}}>
                 Start by Building Connections, <span className="brand-on-background-medium">Finish with Lasting Impressions.</span>
               </InlineCode>
+
               <Heading
                 wrap="balance"
                 variant="display-strong-s">
+
                 <span className="font-code">
                   <LetterFx
                     trigger="instant">
                     Helping Brands Tell Stories and Stories Build Brands.
                   </LetterFx>
                 </span>
+
               </Heading>
+
               <Badge
                 arrow
                 effect
                 href={wtsp}>
                 Book an appointment 
               </Badge>
+
             </Flex>
+
           </Flex>
+
         </Flex>
+
       </Flex>
+      
     </Flex>
   );
 }
