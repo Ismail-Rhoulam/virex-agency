7149d426e4dfdd7ab1b43f6dbb8cdbbf813ba630
Ismail RHOULAM <150340166+Ismail-Rhoulam@users.noreply.github.com>
2024-11-07 15:38:44 +0000

    correction de styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ export const Header = () => {
 
         <Flex
             as="header"
-            className={style.position}
+            className={styles.position}
             background="surface" border="neutral-medium" borderStyle="solid-1" radius="m-4" shadow="l"
             padding="4" justifyContent="center">
             <Flex
