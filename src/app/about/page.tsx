804d9ea375c41152ba8ed30e5ca649ca693977cf
Ismail RHOULAM <150340166+Ismail-Rhoulam@users.noreply.github.com>
2024-11-09 00:31:08 +0100

    Testing with only Flex

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,6 @@
 import { render } from '@/app/resources'
-import { Flex, Grid, Logo, Text, Heading } from '@/ui/components';
+import { Flex, Grid, Logo, Text, Heading, RevealFx } from '@/ui/components';
+import { about } from '../resources/content';
 
 
 export async function generateMetadata() {
@@ -14,16 +15,24 @@ export async function generateMetadata() {
 
 export default function About() {
     return (
-        <Grid columns="repeat(3, 1fr)" fillWidth align="center" padding='l' zIndex="1">
-            <Flex direction="column" background="neutral-medium" fillWidth >
-                <Heading>Get To Know Us!</Heading>
+        <Flex fillWidth gap='m' paddingY='l' direction='column' zIndex="1">
+            <Flex direction='row' justifyContent="start" paddingLeft='l' gap='24'>
+                <Flex>
+                    <RevealFx translateY="4">
+                        <Heading>{about.description}</Heading>
+                    </RevealFx>
+                </Flex>
+                <Flex>
+                    <RevealFx>
+                        <Text>I'm Glad You Came This Far!!</Text>
+                    </RevealFx>
+                </Flex>
+                <Flex>
+                    <RevealFx>
+                        <Text>This is where it get's intresting!!!</Text>
+                    </RevealFx>
+                </Flex>
             </Flex>
-            <Flex direction="column" fillWidth >
-                <Heading>Test</Heading>
-            </Flex>
-            <Flex direction="column" background="neutral-medium" fillWidth >
-                <Heading>Test</Heading>
-            </Flex>
-        </Grid>
+        </Flex>
     )
 };
