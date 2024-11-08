e9ddff413de40f74bf180d83c4fb0ef1522f35dd
Ismail RHOULAM <150340166+Ismail-Rhoulam@users.noreply.github.com>
2024-11-08 14:23:09 +0000

    Removale of the last Flex in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -45,7 +45,7 @@ export const Footer = () => {
                 </Flex>
 
             </Flex>
-            <Flex height="80" show='s'></Flex>
+            {/* <Flex height="80" show='s'></Flex> */}
         </Flex>
     )
 }
