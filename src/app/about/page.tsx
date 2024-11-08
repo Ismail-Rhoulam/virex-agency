6828b54696bc76f7c622266867660d252f0e0223
Ismail RHOULAM <150340166+Ismail-Rhoulam@users.noreply.github.com>
2024-11-08 12:07:34 +0000

    Update about/page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,25 @@
-import { Flex, Text } from '@/ui/components';
+import { render } from '@/app/resources'
+import { Flex, Logo, Text, Heading } from '@/ui/components';
+
+
+export async function generateMetadata() {
+    const { home } = render();
+    const title = home.title;
+    const description = home.description;
+    return {
+      title,
+      description
+    }
+  }
 
 export default function About() {
     return (
-        <Flex>
-            <Text>About</Text>
+        <Flex
+            fillWidth fillHeight border="surface">
+            <Logo size='s'/>
+            <Heading>
+                <Text>Get To Know Us</Text>
+            </Heading>
         </Flex>
     )
 };
