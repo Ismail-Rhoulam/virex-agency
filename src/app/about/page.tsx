f09b06ff7bd27c59058fa086d46c4ffc3b4b441c
Ismail RHOULAM <150340166+Ismail-Rhoulam@users.noreply.github.com>
2024-11-09 00:03:10 +0100

    Try to understand Flex and Grid behavior

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -15,13 +15,13 @@ export async function generateMetadata() {
 export default function About() {
     return (
         <Grid columns="repeat(3, 1fr)" fillWidth>
-            <Flex direction="column" background="neutral-medium">
+            <Flex direction="column" background="neutral-medium" fillWidth justifyContent="center">
                 <Heading>Get To Know Us!</Heading>
             </Flex>
-            <Flex direction="column" background="neutral-medium">
+            <Flex direction="column" fillWidth justifyContent="center">
                 <Heading>Test</Heading>
             </Flex>
-            <Flex direction="column" background="neutral-medium">
+            <Flex direction="column" background="neutral-medium" fillWidth justifyContent="center">
                 <Heading>Test</Heading>
             </Flex>
         </Grid>
