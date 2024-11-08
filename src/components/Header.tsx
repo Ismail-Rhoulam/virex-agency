28792d12385d5e015222fcf35c8f4a95f7124229
Ismail RHOULAM <150340166+Ismail-Rhoulam@users.noreply.github.com>
2024-11-08 13:35:58 +0000

    Majore update in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,64 +12,74 @@ export const Header = () => {
     const { home, about, services, portfolio } = render();
 
     return (
+        <>
+            <Flex
+                className={styles.mask}
+                position="fixed" zIndex={9}
+                fillWidth minHeight="80" justifyContent="center">
+            </Flex>
 
-        <Flex
-            as="header"
-            style={{width: 'fit-content'}}
-            className={styles.position}
-            background="surface" border="neutral-medium" borderStyle="solid-1" radius="m-4" shadow="l"
-            padding="4" justifyContent="center">
-
-            <Flex direction="row" justifyContent="space-between">
-
-                <Flex>
-                    <Logo size="s" icon={false}/>
-                </Flex>
+            <Flex
+                as="header"
+                style={{width: 'fit-content'}}
+                className={styles.position} fillWidth
+                padding="8" justifyContent="center">
                 
                 <Flex
-                    gap="4"
+                    paddingLeft="12" fillWidth
+                    alignItems="center"
                     textVariant="body-default-s">
-                    { routes['/'] && (
-                        <ToggleButton
-                            href="/"
-                            size="m"
-                            prefixIcon="home"
-                            label={home.label}
-                            selected={pathname === '/'}
-                            />
-                    )}
-                    { routes['/about'] && (
-                        <ToggleButton
-                            href="/about"
-                            size="m"
-                            prefixIcon="person"
-                            label={about.label}
-                            selected={pathname === '/about'}
-                            />
-                    )}
-                    { routes['/services'] && (
-                        <ToggleButton
-                            href="/services"
-                            size="m"
-                            prefixIcon="grid"
-                            label={services.label}
-                            selected={pathname === '/services'}
-                            />
-                    )}
-                    { routes['/porfolio'] && (
-                        <ToggleButton
-                            href="/portfolio"
-                            size="m"
-                            prefixIcon="gallery"
-                            label={portfolio.label}
-                            selected={pathname === '/portfolio'}
-                            />
-                    )}
+                    <Logo size="s" icon={false}/>
                 </Flex>
-                
-                <Flex></Flex>
-
+                <Flex fillWidth justifyContent="center">
+                    <Flex
+                        background="surface" border="neutral-medium" borderStyle="solid-1" radius="m-4" shadow="l" padding="4" justifyContent="center">
+                        <Flex
+                            gap="4"
+                            textVariant="body-default-s">
+                            { routes['/'] && (
+                                <ToggleButton
+                                    href="/"
+                                    size="m"
+                                    prefixIcon="home"
+                                    label={home.label}
+                                    selected={pathname === '/'}
+                                    />
+                            )}
+                            { routes['/about'] && (
+                                <ToggleButton
+                                    href="/about"
+                                    size="m"
+                                    prefixIcon="person"
+                                    label={about.label}
+                                    selected={pathname === '/about'}
+                                    />
+                            )}
+                            { routes['/services'] && (
+                                <ToggleButton
+                                    href="/services"
+                                    size="m"
+                                    prefixIcon="grid"
+                                    label={services.label}
+                                    selected={pathname === '/services'}
+                                    />
+                            )}
+                            { routes['/porfolio'] && (
+                                <ToggleButton
+                                    href="/portfolio"
+                                    size="m"
+                                    prefixIcon="gallery"
+                                    label={portfolio.label}
+                                    selected={pathname === '/portfolio'}
+                                    />
+                            )}
+                        </Flex>
+                    </Flex>
+                </Flex>
+                <Flex fillWidth justifyContent="flex-end" alignItems="center">
+                </Flex>
             </Flex>
-        </Flex>
+
+        </>
     )
 }
