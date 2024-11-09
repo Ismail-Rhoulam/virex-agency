930b51762cb363224650d7c1d14effbfebe1fc7d
Ismail RHOULAM <150340166+Ismail-Rhoulam@users.noreply.github.com>
2024-11-09 14:56:29 +0000

    HAAAAAAA

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -15,7 +15,7 @@ export async function generateMetadata() {
 export default function Portfolio() {
     return (
         <Flex fillWidth justifyContent='center'>
-          <Flex justifyContent='fit-content' maxHeight="l" maxWidth="m">
+          <Flex justifyContent='fit-content' maxHeight="l" maxWidth="m" background='neutral-medium'>
             <Spline content="" scene="https://prod.spline.design/wmxzs5LVPn1oQQGq/scene.splinecode"/>
           </Flex>
         </Flex>
