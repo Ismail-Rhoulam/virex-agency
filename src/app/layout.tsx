a612d46356b370a9ab2ceca77f4fc1e610478c5c
Ismail RHOULAM <150340166+Ismail-Rhoulam@users.noreply.github.com>
2024-11-10 02:50:46 +0000

    asdasdadada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,38 +40,67 @@ const code = Source_Code_Pro({
 	display: 'swap',
 });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <Flex
-            as="html"
-            lang="en"
-            fillHeight
-            background="page"
-            className={classNames(
-                primary.variable,
-                secondary.variable,
-                tertiary.variable,
-                code.variable,
-                'root'
-            )}
-        >
-            <Flex as="body" direction="column" fillWidth margin="0" padding="0">
-                <Header /> {/* Move header to be directly inside the body */}
-                <Background
-                    mask="cursor"
-                    gradient={{ display: true }}
-                    dots={{ display: true }}
-                    lines={{ display: true }}
-                />
-                <Flex fillWidth paddingY="l" paddingX="l" justifyContent="center" flex={1} zIndex={1}>
-                    <Flex justifyContent="center" fillWidth minHeight="0">
-                        {children}
-                        <Analytics />
-                        <SpeedInsights />
-                    </Flex>
-                </Flex>
-                <Footer />
-            </Flex>
-        </Flex>
-    );
+export default function RootLayout({
+  	children,
+}: Readonly<{
+  	children: React.ReactNode;
+}>) {
+	return (
+		<Flex
+			as="html" lang="en"
+			fillHeight background="page"
+			data-theme="dark"
+			data-brand="blue"
+			data-accent="orange"
+			data-neutral="slate"
+			data-border="playful"
+			data-solid="contrast"
+			data-solid-style="flat"
+			data-surface="translucent"
+			data-transition="all"
+			className={classNames(
+				primary.variable,
+				secondary.variable,
+				tertiary.variable,
+				code.variable,
+				'root')}>
+			<Flex
+				as="body"
+				direction="column"
+				fillWidth fillHeight margin="0" padding="0">
+
+				<Background
+						mask="cursor"
+						gradient={{
+							display: true
+						  }}
+						dots={{
+							display: true
+						  }}
+						lines={{
+							display: true
+						  }}/>
+				<Flex
+						fillWidth
+						minHeight="16">
+				</Flex>
+				
+				<Header/>
+				
+				<Flex fillWidth paddingY="l" paddingX="l"
+					justifyContent="center" flex={1} zIndex={1}>
+					<Flex 
+						justifyContent="center"
+						fillWidth minHeight="0">
+						{children}
+						<Analytics />
+						<SpeedInsights />
+					</Flex>
+				</Flex>
+
+				<Footer />
+
+			</Flex> {/* body */}
+		</Flex>
+	);
 }
