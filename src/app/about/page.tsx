fb5383035876de0382173010e18fa7f154636fb6
Ismail RHOULAM <150340166+Ismail-Rhoulam@users.noreply.github.com>
2024-11-10 01:41:53 +0000

    mmajore

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -29,77 +29,78 @@ export default function About() {
                     as-='main'
                     direction='column' justifyContent='center'
                     fillWidth fillHeight gap='l'>
-                        
-                    <Flex
-                        fillWidth
-                        direction='column' 
-                        alignItems='center' 
-                        justifyContent='center'
-                        borderStyle='solid-1'
-                        style={{minHeight: '80vh'}}> {/* Section 1 */}
 
-                            <Flex
-                                paddingBottom='m'>
+                    <Flex fillHeight fillWidth>
+                        <Flex
+                            fillWidth
+                            direction='column' 
+                            alignItems='center' 
+                            justifyContent='center'
+                            borderStyle='solid-1'
+                            style={{minHeight: '80vh'}}> {/* Section 1 */}
 
-                                <Heading wrap='balance' variant='display-strong-l' style={{textAlign: "center"}}>
-                                    <SparkleFx>
-                                        <span>Get to know Us</span>
-                                    </SparkleFx>
-                                </Heading>
+                                <Flex
+                                    paddingBottom='m'>
+
+                                    <Heading wrap='balance' variant='display-strong-l' style={{textAlign: "center"}}>
+                                        <SparkleFx>
+                                            <span>Get to know Us</span>
+                                        </SparkleFx>
+                                    </Heading>
+
+                                </Flex>
+                                
+                                <IconButton
+                                    size="l"
+                                    href='#anchor1'
+                                    variant="ghost"
+                                    icon="chevronDown"
+                                    />
+
+                        </Flex> {/* Section 1 */}
+
+                        <Flex
+                            fillWidth
+                            direction='column' 
+                            alignItems='center' 
+                            justifyContent='center'
+                            borderStyle='solid-1'
+                            style={{minHeight: '80vh'}}> {/* Section 2 */}
+
+                            <Flex id='anchor1' fillWidth justifyContent='start' alignItems='center' style={{minHeight: "75vh"}}
+                                borderStyle='solid-1' direction='column' gap='24'>
+
+                                <Flex
+                                    style={{height: 'fit-content'}}>
+                                    <Heading wrap='balance' variant='display-strong-m' padding='l' style={{textAlign: "center"}}>
+                                        <SparkleFx>
+                                            <span>Our Philosophy</span>
+                                        </SparkleFx>
+                                    </Heading>
+                                </Flex>
+
+                                <Flex
+                                    gap='24'
+                                    direction='column'
+                                    fillHeight maxWidth='l'
+                                    justifyContent='center'>
+
+                                    <Text
+                                        paddingBottom='8'
+                                        variant='body-default-xl'>
+                                        VIREX Agency is a versatile agency that combines marketing, production, and communication expertise to create dynamic and adaptable solutions that resonate with your audience. No matter your industry or vision, VIREX is equipped to amplify your voice and craft stories that make a lasting impression.
+                                    </Text>
+                                    <Text
+                                        paddingBottom='8'
+                                        variant='body-default-xl'>
+                                        At the heart of Virex Agency lies the belief that collaboration fuels success. We work closely with our clients, aligning our creative vision with their business objectives to produce results that make a lasting impact. Every project is treated with care, ensuring that the final product reflects both your brand’s essence and our commitment to excellence.
+                                    </Text>
+                                </Flex>
 
                             </Flex>
                             
-                            <IconButton
-                                size="l"
-                                href='#anchor1'
-                                variant="ghost"
-                                icon="chevronDown"
-                                />
-
-                    </Flex> {/* Section 1 */}
-
-                    <Flex
-                        fillWidth
-                        direction='column' 
-                        alignItems='center' 
-                        justifyContent='center'
-                        borderStyle='solid-1'
-                        style={{minHeight: '80vh'}}> {/* Section 2 */}
-
-                        <Flex id='anchor1' fillWidth justifyContent='start' alignItems='center' style={{minHeight: "80vh"}}
-                            borderStyle='solid-1' direction='column' gap='24'>
-
-                            <Flex
-                                style={{height: 'fit-content'}}>
-                                <Heading wrap='balance' variant='display-strong-m' padding='xl' style={{textAlign: "center"}}>
-                                    <SparkleFx>
-                                        <span>Our Philosophy</span>
-                                    </SparkleFx>
-                                </Heading>
-                            </Flex>
-
-                            <Flex
-                                gap='24'
-                                direction='column'
-                                fillHeight maxWidth='l'
-                                justifyContent='center'>
-                                    
-                                <Text
-                                    paddingBottom='8'
-                                    variant='body-default-xl'>
-                                    VIREX Agency is a versatile agency that combines marketing, production, and communication expertise to create dynamic and adaptable solutions that resonate with your audience. No matter your industry or vision, VIREX is equipped to amplify your voice and craft stories that make a lasting impression.
-                                </Text>
-                                <Text
-                                    paddingBottom='8'
-                                    variant='body-default-xl'>
-                                    At the heart of Virex Agency lies the belief that collaboration fuels success. We work closely with our clients, aligning our creative vision with their business objectives to produce results that make a lasting impact. Every project is treated with care, ensuring that the final product reflects both your brand’s essence and our commitment to excellence.
-                                </Text>
-                            </Flex>
-
-                        </Flex>
-                        
-                    </Flex> {/* Section 2 */}
-
+                        </Flex> {/* Section 2 */}
+                    </Flex>
                 </Flex>
 
             </Flex>
