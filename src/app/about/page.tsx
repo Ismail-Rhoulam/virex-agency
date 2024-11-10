469a1720c0b19c1e54433db2701a295f93c7cc1b
Ismail RHOULAM <150340166+Ismail-Rhoulam@users.noreply.github.com>
2024-11-10 03:03:35 +0000

    tex

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -92,8 +92,41 @@ export default function About() {
                                 </Flex>
 
                             </Flex>
+                            <Flex id='anchor' fillWidth justifyContent='start' alignItems='center'
+                                borderStyle='solid-1' direction='column' gap='24' padding='xl'>
 
-                        </Flex> {/* Section 1 */}
+                                <Flex
+                                    style={{height: 'fit-content'}}>
+                                    <Heading wrap='balance' variant='display-strong-m' padding='l' style={{textAlign: "center"}}>
+                                        <SparkleFx>
+                                            <span>Our Philosophy</span>
+                                        </SparkleFx>
+                                    </Heading>
+                                </Flex>
+
+                                <Flex
+                                    gap='24'
+                                    direction='column'
+                                    fillHeight maxWidth='l'
+                                    justifyContent='center'>
+
+                                    <Text
+                                        align='center'
+                                        paddingBottom='8'
+                                        variant='body-default-xl'>
+                                        VIREX Agency is a versatile agency that combines marketing, production, and communication expertise to create dynamic and adaptable solutions that resonate with your audience. No matter your industry or vision, VIREX is equipped to amplify your voice and craft stories that make a lasting impression.
+                                    </Text>
+                                    <Text
+                                        align='center'
+                                        paddingBottom='8'
+                                        variant='body-default-xl'>
+                                        At the heart of Virex Agency lies the belief that collaboration fuels success. We work closely with our clients, aligning our creative vision with their business objectives to produce results that make a lasting impact. Every project is treated with care, ensuring that the final product reflects both your brand’s essence and our commitment to excellence.
+                                    </Text>
+                                </Flex>
+
+                            </Flex>
+
+                        </Flex>
 
                     </Flex>
                 </Flex>
